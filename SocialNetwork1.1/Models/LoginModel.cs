c447b5965620043b6d46becabfecf4d002d5677a
ByMyTry <anton.taleckij@gmail.com>
2016-11-30 04:52:07 +0300

    All project

---
+++ SocialNetwork1.1/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork1._1.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ SocialNetwork1.1/Models/RegisterModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace SocialNetwork1._1.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string SurName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BirthDay { get; set; }
+        
+        public HttpPostedFileBase Photo { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords dont match")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
+    }
+}
